50b1871e831b2c295331f3337670e460a23be1e1
AjayYadavAi <proajayyadav0525@gmail.com>
2020-05-06 08:45:19 +0530

    Final changes

--- polls/urls.py
+++ polls/urls.py
@@ -5,9 +5,9 @@ app_name = 'polls'
 urlpatterns=[
 	path('',views.IndexView.as_view(), name='index'),
 	# ex: /polls/5/
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
+    path('polls/<int:pk>/results/', views.ResultView.as_view(), name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
